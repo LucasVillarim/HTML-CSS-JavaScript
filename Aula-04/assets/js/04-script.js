133e06954c87369c41365a511158d6150b8d8cb9
LucasVillarim <villarim94@gmail.com>
2021-01-24 15:22:05 -0300

    aula-04 improvements

--- Aula-04/assets/js/04-script.js
+++ Aula-04/assets/js/04-script.js
@@ -1,47 +1,56 @@
 const baseURL = "https://api.chucknorris.io/jokes/";
+const searchURL = 'https://api.chucknorris.io/jokes/search?query=';
 
 const body = document.querySelector('#app');
 
-function card() {
-
+function card(joke) {
+  const cardTemplate = 
+  `
+  <div class="card col-md-3 col-lg-3 m-3" style="width: 18rem;">
+    <div class="card-body">
+      <div class="d-flex align-items-center">
+        <img src="${joke.icon_url}" alt="icone" />
+        <h5 class="card-title ml-3">Jokes</h5>
+      </div>
+      <p class="card-text">${joke.value}</p>
+      <a href="${joke.url}" class="card-link text-uppercase fw-bold">Ver no Site</a>
+    </div>
+  </div>
+  `
+  return cardTemplate;
 }
 
+// Search by category;
 function handleCategory(element) {
-  // const config = {headers: {'Access-Control-Allow-Origin': '*'}};
   axios.get(baseURL + "search?query=" + element).then(res => {
     console.log(res);
-    const selectedCategory = res.data.result.map(joke => {
-      const cardTemplate = 
-      `
-      <div class="card col-md-3 col-lg-3 m-3" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">${joke.value}</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-      `  
-      return cardTemplate;
-    })
+    const selectedCategory = res.data.result.map(joke => card(joke)).join('');
     body.innerHTML = selectedCategory;
   })
 }
 
-
-
-
 axios.get(baseURL + "categories").then(res => {
   const options = res.data.map(name => {
-    const button = `<button class="btn btn-primary m-2" type="button" onclick="handleCategory('${name}')">${name}</button>`
+    const button = `<button class="category-button btn btn-primary m-2" type="button" onclick="handleCategory('${name}')">
+                      ${name}
+                    </button>
+                  `;
     return button;
   }).join('');
     const menu = document.getElementById('menu');
     menu.innerHTML = options;
-  })
+});
   
-  
+// Input Search;
+const sendSearch = document.getElementById('sendForm');
+sendSearch.addEventListener('click', () => {
+  const input = document.getElementById('inputSearch').value;
+  axios.get(`${searchURL}${input}`).then(res => {
+    const searchReturn = res.data.result.map(joke => card(joke)).join('');
+    body.innerHTML = searchReturn;
+  });
+});
+
   
 // axios.get(baseURL).then(
 //   res => {
